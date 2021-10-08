b00199fad43246eba0539bd2e10a45d36160b3eb
iPouponYu <ipouponyuu@gmail.com>
2021-10-08 03:33:19 +0200

    Added simple player's score calculation

--- Blackjack.py
+++ Blackjack.py
@@ -1,17 +1,28 @@
 import random
 
-cards = {2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'J':10, 'Q':10, 'K':10, 'Ace':[1, 10]}
+
+cards = {2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'J':10, 'Q':10, 'K':10, 'Ace1':1, 'Ace10':10}
 
 class PlayerHand:
+
     player_card = random.choices(list(cards.values()))
     player_card_2 = random.choices(list(cards.values()))
+    print(player_card)
+    print(player_card_2)
 
     def __repr__(self):
         return str(self.player_card + self.player_card_2)  
 
 
+def score():
+    playerscore = PlayerHand.player_card_2[0] + PlayerHand.player_card[0]
+    return playerscore
+
+
+
+
 player_cards = PlayerHand()
-print(player_cards)
+print(score())
 
 
 
