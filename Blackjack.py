8ebc5803b767992e881dd22133bb82b973dcbc7f
iPouponYu <ipouponyuu@gmail.com>
2021-10-08 03:42:21 +0200

    Added simple croupier's hand

--- Blackjack.py
+++ Blackjack.py
@@ -1,17 +1,23 @@
 import random
 
 
-cards = {2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'J':10, 'Q':10, 'K':10, 'Ace1':1, 'Ace10':10}
+cards = {2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'J':10, 'Q':10, 'K':10, 'Ace1':1, 'Ace10':11}
 
 class PlayerHand:
 
     player_card = random.choices(list(cards.values()))
     player_card_2 = random.choices(list(cards.values()))
-    print(player_card)
-    print(player_card_2)
+    print('Your hand is: ' + str(player_card)+ str(player_card_2))
+    
 
     def __repr__(self):
         return str(self.player_card + self.player_card_2)  
+class CroupierHand:
+        croupier_card = random.choices(list(cards.values()))
+        print('Croupier hand is: ' + str(croupier_card))
+class CroupierFlip:
+    croupier_card_2 = random.choices(list(cards.values()))
+    print('Croupier hand is: ' + str(croupier_card_2))
 
 
 def score():
@@ -21,7 +27,8 @@ def score():
 
 
 
-player_cards = PlayerHand()
+PlayerHand()
+CroupierHand()
 print(score())
 
 
